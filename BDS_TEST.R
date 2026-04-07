e935eab325c251b84ad4b89553a51d2259b36e75
Sebastian Küppers <sebastian_kueppers@outlook.de>
2026-04-07 14:05:51 +0200

    summarize simulation results

--- BDS_TEST.R
+++ BDS_TEST.R
@@ -223,75 +223,76 @@ process_subject_ar <- function(subject_df) {
 ## ----------------------------------
 # Apply BDS test: AR ----------------
 
-# Count total iterations for progress bar
-total_iterations <- length(names(DATA.AR)) *
-  length(names(DATA.AR[[1]])) *
-  length(names(DATA.AR[[1]][[1]])) *
-  length(names(DATA.AR[[1]][[1]][[1]]))
+# # Count total iterations for progress bar
+# total_iterations <- length(names(DATA.AR)) *
+#   length(names(DATA.AR[[1]])) *
+#   length(names(DATA.AR[[1]][[1]])) *
+#   length(names(DATA.AR[[1]][[1]][[1]]))
+# 
+# # Initialise progress bar
+# pb <- txtProgressBar(
+#   min   = 0,
+#   max   = total_iterations,
+#   style = 3
+# )
+# 
+# iter <- 0
+# 
+# # Initialize timestamp
+# time_start_ar <- Sys.time()
+# cat("\n[", format(time_start_ar, "%Y-%m-%d %H:%M:%S"), "]",
+#     "Starting AR loop...\n")
+# 
+# RESULTS.AR <- list()
+# 
+# for (n_key in names(DATA.AR)) {
+#   RESULTS.AR[[n_key]] <- list()
+#   
+#   for (t_key in names(DATA.AR[[n_key]])) {
+#     RESULTS.AR[[n_key]][[t_key]] <- list()
+#     
+#     if (t_key == "T500") {
+#       iter <- iter + 1
+#       setTxtProgressBar(pb, iter)
+#       next
+#     }
+#     
+#     for (phi_key in names(DATA.AR[[n_key]][[t_key]])) {
+#       RESULTS.AR[[n_key]][[t_key]][[phi_key]] <- list()
+#       
+#       for (sigma_key in names(DATA.AR[[n_key]][[t_key]][[phi_key]])) {
+#         
+#         raw_data <- DATA.AR[[n_key]][[t_key]][[phi_key]][[sigma_key]]$likert_1_100
+#         
+#         # Parallelise over subjects within each condition
+#         condition_results <- furrr::future_map(
+#           raw_data,
+#           process_subject_ar,
+#           .options = furrr::furrr_options(seed = 2310)
+#         )
+#         
+#         RESULTS.AR[[n_key]][[t_key]][[phi_key]][[sigma_key]] <- condition_results
+#         
+#         iter <- iter + 1
+#         setTxtProgressBar(pb, iter)
+#       }
+#     }
+#   }
+# }
+# 
+# close(pb)
+# 
+# time_end_ar <- Sys.time()
+# 
+# cat("\n[", format(time_end_ar, "%Y-%m-%d %H:%M:%S"), "]",
+#     "AR loop complete. Time elapsed:",
+#     round(difftime(time_end_ar, time_start_ar, units = "mins"), 2),
+#     "minutes\n")
+# 
+# # Save
+# saveRDS(RESULTS.AR, file = "RESULTS_AR.rds")
 
-# Initialise progress bar
-pb <- txtProgressBar(
-  min   = 0,
-  max   = total_iterations,
-  style = 3
-)
-
-iter <- 0
-
-# Initialize timestamp
-time_start_ar <- Sys.time()
-cat("\n[", format(time_start_ar, "%Y-%m-%d %H:%M:%S"), "]",
-    "Starting AR loop...\n")
-
-RESULTS.AR <- list()
-
-for (n_key in names(DATA.AR)) {
-  RESULTS.AR[[n_key]] <- list()
-  
-  for (t_key in names(DATA.AR[[n_key]])) {
-    RESULTS.AR[[n_key]][[t_key]] <- list()
-    
-    if (t_key == "T500") {
-      iter <- iter + 1
-      setTxtProgressBar(pb, iter)
-      next
-    }
-    
-    for (phi_key in names(DATA.AR[[n_key]][[t_key]])) {
-      RESULTS.AR[[n_key]][[t_key]][[phi_key]] <- list()
-      
-      for (sigma_key in names(DATA.AR[[n_key]][[t_key]][[phi_key]])) {
-        
-        raw_data <- DATA.AR[[n_key]][[t_key]][[phi_key]][[sigma_key]]$likert_1_100
-        
-        # Parallelise over subjects within each condition
-        condition_results <- furrr::future_map(
-          raw_data,
-          process_subject_ar,
-          .options = furrr::furrr_options(seed = 2310)
-        )
-        
-        RESULTS.AR[[n_key]][[t_key]][[phi_key]][[sigma_key]] <- condition_results
-        
-        iter <- iter + 1
-        setTxtProgressBar(pb, iter)
-      }
-    }
-  }
-}
-
-close(pb)
-
-time_end_ar <- Sys.time()
-
-cat("\n[", format(time_end_ar, "%Y-%m-%d %H:%M:%S"), "]",
-    "AR loop complete. Time elapsed:",
-    round(difftime(time_end_ar, time_start_ar, units = "mins"), 2),
-    "minutes\n")
-
-# Save
-saveRDS(RESULTS.AR, file = "RESULTS_AR.rds")
-
+RESULTS.AR <- readRDS(file = "RESULTS_AR.rds")
 
 ## ----------------------------------
 # Reset to Sequential Processing ----
@@ -315,7 +316,27 @@ summary_df <- expand.grid(
       na.rm = TRUE
     )
   ) |>
-  dplyr::ungroup()
+  dplyr::ungroup() |>
+  mutate(
+    T_numeric = as.numeric(gsub("T", "", T)),
+    Phi_numeric = as.numeric(gsub("Phi", "", Phi)),
+    Sigma_numeric = as.numeric(gsub("Sigma", "", Sigma))
+  )
 
 print(summary_df, n = 100)
 
+# We expect a rejection rate of 0.05 which is the alpha level of the test:
+mean(summary_df$rejection_rate) #0.04666667
+
+# Three-way ANOVA for main effect of any parameter 
+model <- aov(rejection_rate ~ T + Phi + Sigma, 
+             data = summary_df)
+
+summary(model)
+#             Df  Sum Sq   Mean Sq F value Pr(>F)
+# T            5 0.00388 0.0007767   1.687  0.147
+# Phi          3 0.00050 0.0001667   0.362  0.781
+# Sigma        3 0.00148 0.0004944   1.074  0.365
+# Residuals   84 0.03867 0.0004603
+
+# rejection_rate does not vary over T, Phi, or Sigma
